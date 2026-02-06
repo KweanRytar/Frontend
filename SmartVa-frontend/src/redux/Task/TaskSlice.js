63c7ffd3f3c6ff72a60d64adeaf81ba1669a12eb
Rytar <ritanoblejane@gmail.com>
2026-02-06 15:42:03 +0100

    fixed fetching of events

--- SmartVa-frontend/src/redux/Task/TaskSlice.js
+++ SmartVa-frontend/src/redux/Task/TaskSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery  } from '@reduxjs/toolkit/query/react';
 export const taskApi = createApi({
   reducerPath: 'taskApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://smartva-backend-file.onrender.com/task',
+    baseUrl:'https://smartva-services.up.railway.app',
     credentials: 'include'
   }),
   tagTypes: ['Task'],
