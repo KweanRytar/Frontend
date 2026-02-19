14b7df5cd7da5dc34d81265dd7ed193e1682d4fb
Rytar <ritanoblejane@gmail.com>
2026-02-19 12:25:10 +0100

    fixed editor position

--- SmartVa-frontend/src/pages/CreateNote.jsx
+++ SmartVa-frontend/src/pages/CreateNote.jsx
@@ -89,7 +89,7 @@ const CreateNote = () => {
       </div>
 
       {/* EDITOR */}
-      <div className="pt-32 md:pt-40 p-4 md:p-6">
+      <div className="pt-44 md:pt-48 p-4 md:p-6">
         <Editor onChange={handleEditorChange} />
       </div>
     </form>
--- SmartVa-frontend/src/pages/EditNote.jsx
+++ SmartVa-frontend/src/pages/EditNote.jsx
@@ -95,7 +95,7 @@ const EditNote = () => {
       </div>
 
       {/* EDITOR */}
-      <div className="pt-32 md:pt-40 p-4 md:p-6">
+      <div className="pt-44 md:pt-48 p-4 md:p-6">
         <Editor
           initialHtml={noteData.contentHtml}
           onChange={handleEditorChange}
